19ce713273e28fc2b510b533dc94b915920c7aa9
Oliver <oliver@tonnesen.com>
2019-01-08 11:09:42 -0800

    Nat please fix

--- game/game.go
+++ game/game.go
@@ -38,13 +38,19 @@ func (g *Game) applyMove(m *api.MoveRequest) {
 func (g *Game) CheckVictory() int{
 
 	if(g.Board[0][0]!=0){
-		if(g.Board[0][0] == g.Board[0][1] && g.Board[0][2] == g.Board[0][0]) return g.Board[0][0]
-		else return 0
+		if(g.Board[0][0] == g.Board[0][1] && g.Board[0][2] == g.Board[0][0]) {
+			return g.Board[0][0]
+		} else {
+			return 0
+		}
 	}
 
 	if(g.Board[1][0]!=0){
-		if(g.Board[1][0] == g.Board[1][1] && g.Board[1][2] == g.Board[1][0]) return g.Board[1][0]
-		else return 0
+		if(g.Board[1][0] == g.Board[1][1] && g.Board[1][2] == g.Board[1][0]) {
+			return g.Board[1][0]
+		} else {
+			return 0
+		}
 	}
 
 	if(g.Board[2][0]!=0){
