ac7aaad4b5d5c6122e963c6eea2c18c8fa3af08b
Nat <natdring97@gmail.com>
2019-01-09 11:46:58 -0800

    copied some stuff from LoanShark and added colours to drawX and drawO

--- static/game.js
+++ static/game.js
@@ -34,12 +34,14 @@ gameArea.ctx.fill()
 function drawX(x, y) {
 	gameArea.ctx.beginPath();
 	gameArea.ctx.font = 2*gameArea.canvas.width/7 +'px Arial';
+	gameArea.ctx.fillStyle = "#2874A6";
 	gameArea.ctx.fillText('X', gameArea.canvas.width/6 - 1.7*gameArea.canvas.width/18 + x*gameArea.canvas.width/3, gameArea.canvas.height/6 + 2*gameArea.canvas.width/18 + y*gameArea.canvas.height/3);
 }
 
 function drawO(x, y) {
 	gameArea.ctx.beginPath();
 	gameArea.ctx.font = 2*gameArea.canvas.width/7 +'px Arial';
+	gameArea.ctx.fillStyle = "#7B241C";
 	gameArea.ctx.fillText('O', gameArea.canvas.width/6 - 2*gameArea.canvas.width/18 + x*gameArea.canvas.width/3, gameArea.canvas.height/6 + 2*gameArea.canvas.width/18 + y*gameArea.canvas.height/3);
 }
 
@@ -72,3 +74,34 @@ function handleClick(e) {
 	}
 	
 }
+
+//taken from loanshark
+
+let getJSON = function (url, params, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        const status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send(params);
+};
+
+let getData = function (url, params) {
+    getJSON(url, params,
+        function (err, data) {
+            if (err !== null) {
+                console.log("Error retrieving data: " + err);
+            } else {
+                login(data['validate']);
+            }
+            //document.getElementById('submit').style.cursor = '';
+        });
+};
